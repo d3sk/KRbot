36ec95de72532ad2b9a2b1b23a5615fdb7706fc1
Robert Cooper <heliosthefirstwastaken@gmail.com>
2018-03-18 00:56:55 +1100

    Removed reference comment that I accidentally forgot to remove in 9003cf7

--- utility.py
+++ utility.py
@@ -1,16 +1,6 @@
 from discord import Message, Member
 
 
-# def format_string_with_message_data(string: str, message: Message) -> str:
-#     return string\
-#         .replace('{{username}}', message.author.name)\
-#         .replace('{{userid}}', str(message.author.id))\
-#         .replace('{{usertag}}', message.author.discriminator)\
-#         .replace('{{usernick}}', message.author.nick)\
-#         .replace('{{servername}}', message.guild.name)\
-#         .replace('{{channelname}}', message.channel.name)\
-#         .replace('{{channelid}}', str(message.channel.id))
-
 def __replace(string, replacement_dictionary):
     """
     :param string: Input to be manipulated
