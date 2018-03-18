92957c8c76e827da47eef9646b3326829d6c140e
Robert Cooper <heliosthefirstwastaken@gmail.com>
2018-03-18 16:41:34 +1100

    Stars will now work on any message, regardless of if it's cached

--- listeners.py
+++ listeners.py
@@ -6,14 +6,18 @@ import constants
 from database import moderation, stars
 from discord.ext import commands
 
+__bot: commands.Bot = None
+
 
 def start(bot: commands.Bot):
     print('Adding listeners.')
+    global __bot
+    __bot = bot
     bot.add_listener(delete_messages_in_auto_delete_channels, 'on_message')
     bot.add_listener(anti_ghost, 'on_message')
     bot.add_listener(log.log_standard_action, 'on_command_completion')
     bot.add_listener(post_leave_message, 'on_member_remove')
-    bot.add_listener(listen_for_stars, 'on_reaction_add')
+    bot.add_listener(listen_for_stars, 'on_raw_reaction_add')
 
 
 def remove(bot: commands.Bot):
@@ -49,24 +53,34 @@ async def post_leave_message(member: discord.Member):
     await channel.send(leave_message)
 
 
-async def listen_for_stars(reaction: discord.Reaction, member: discord.Member):
-    if reaction.emoji == '🌟' and reaction.count > 1:
-        star_channel = None
-        for channel in member.guild.text_channels:
-            if channel.name == 'starboard' or channel.name == 'stars':
-                star_channel = channel
-                break
-        if not star_channel:
-            return
-        if not stars.is_starred(reaction.message):
+async def listen_for_stars(emoji: discord.Emoji, message_id: int, channel_id: int, user_id: int):
+    channel = __bot.get_channel(channel_id)
+    message = await channel.get_message(message_id)
+    print(message, channel)
+    # Get the channel we will post the star message in (exit function if none)
+    star_channel = None
+    for channel in message.guild.text_channels:
+        if channel.name == 'starboard' or channel.name == 'stars':
+            star_channel = channel
+            break
+    if not star_channel:
+        return
+    print(star_channel)
+
+    for reaction in message.reactions:
+        print(reaction.emoji)
+        if reaction.emoji == '🌟' and reaction.count >= 2 and not stars.is_starred(message):
+
             embed = discord.Embed(description=reaction.message.content, color=discord.Color.gold())
-            embed.set_author(name=f'{reaction.message.author.name}#{reaction.message.author.discriminator}',
-                             icon_url=reaction.message.author.avatar_url)
-            embed.timestamp = reaction.message.created_at
-            embed.set_footer(text=f'<#{reaction.message.channel.id}>')
+            embed.set_author(name=f'{message.author.name}#{message.author.discriminator}',
+                             icon_url=message.author.avatar_url)
+            embed.timestamp = message.created_at
+            embed.set_footer(text=f'#{message.channel.name}')
             try:
-                embed.set_image(url=reaction.message.attachments[0].url)
+                embed.set_image(url=message.attachments[0].url)
             except (AttributeError, IndexError):
                 pass
             star = await star_channel.send(embed=embed)
-            stars.add(reaction.message, star)
+            print(star)
+            stars.add(message, star)
+            return
